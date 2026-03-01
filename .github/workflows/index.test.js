f116b5f6d52dc128854d3b4746b24a106e6b824f
TrustyBridge <123024855+TrustyBridge@users.noreply.github.com>
2026-03-01 14:00:23 -0500

    Create index.test.js

---
+++ .github/workflows/index.test.js
@@ -0,0 +1,20 @@
+const fs   = require('fs');
+const path = require('path');
+
+function load(file) {
+  const html = fs.readFileSync(path.resolve(__dirname, file), 'utf8');
+  document.open();
+  document.write(html);
+  document.close();
+}
+
+const pages = ['index.html','analysis_methods.html','collection_methods.html','data_sources.html','publication_venues.html'];
+
+test.each(pages)('%s has links to all other pages', (currentpage) => {
+  load(currentPage);
+  const otherPages = pages.filter(p => p!== currentPage);
+  otherPages.forEach(targetPage => {
+    const link = document.querySelector(`a[href="${targetPage}"]`);
+    expect(link).not.toBeNull();
+  });
+});
